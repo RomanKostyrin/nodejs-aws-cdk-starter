5431bf4a039905abbd23c1cc635a68a79b4a4c8f
unknown <enemy-iubip@mail.ru>
2023-10-27 21:25:23 -0700

    init cdk

---
+++ test/nodejs-aws-cdk-starter.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NodejsAwsCdkStarter from '../lib/nodejs-aws-cdk-starter-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nodejs-aws-cdk-starter-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NodejsAwsCdkStarter.NodejsAwsCdkStarterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
